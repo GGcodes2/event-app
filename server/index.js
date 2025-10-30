919834c278a197bdc4dff89cb71a3c46dcd4dbaa
Gaurav Katare <gauravkatare@Gauravs-MacBook-Air.local>
2025-10-30 23:54:32 +0530

    done

--- server/index.js
+++ server/index.js
@@ -41,3 +41,16 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://event-app-zeta-two.vercel.app"
 ];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
